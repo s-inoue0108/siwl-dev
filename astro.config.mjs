c7e4296fd4e4543737f3d7a5048b319e51340348
shota <inoueshota0108@gmail.com>
2024-10-09 23:20:05 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
 
 // Remark/Rehype Plugins from third-party
 import remarkDirective from "remark-directive";
@@ -18,69 +19,62 @@ import rehypeKatex from "rehype-katex";
 import rehypeHeadings from "./src/plugins/rehype/rehype-headings";
 import rehypeInlineAnchor from "./src/plugins/rehype/rehype-inline-anchor";
 
-import tailwind from "@astrojs/tailwind";
-
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  server: {
-      port: 3000,
+	server: {
+		port: 3000,
 	},
 
-  site: "https://siwl.dev",
+	site: "https://siwl.dev",
 
-  integrations: [
-      solid(),
-      sitemap({
-          i18n: {
-              defaultLocale: "ja",
-              locales: {
-                  ja: "ja-JP",
-              },
-          },
-      }),
-      partytown({
-          // Adds dataLayer.push as a forwarding-event.
-          config: {
-              forward: ["dataLayer.push"],
-          },
-      }),
-      tailwind(),
+	integrations: [
+		solid(),
+		tailwind(),
+		sitemap({
+			i18n: {
+				defaultLocale: "ja",
+				locales: {
+					ja: "ja-JP",
+				},
+			},
+		}),
+		partytown({
+			// Adds dataLayer.push as a forwarding-event.
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
 	],
 
-  markdown: {
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-          theme: "dracula-soft",
-      },
-      remarkPlugins: [[rlc, { shortenUrl: true }], remarkDirective, remarkMath, remarkCodeTitles],
-      rehypePlugins: [
-          rehypeRaw,
-          [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }],
-          rehypeInlineAnchor,
-          rehypeHeadings,
-          rehypeKatex,
-      ],
-      remarkRehype: {
-          footnoteLabelTagName: "h1",
-          footnoteLabel: "脚注",
-          footnoteBackLabel: "Return to content",
-      },
-      gfm: true,
+	markdown: {
+		syntaxHighlight: "shiki",
+		shikiConfig: {
+			theme: "dracula-soft",
+		},
+		remarkPlugins: [[rlc, { shortenUrl: true }], remarkDirective, remarkMath, remarkCodeTitles],
+		rehypePlugins: [
+			rehypeRaw,
+			[rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }],
+			rehypeInlineAnchor,
+			rehypeHeadings,
+			rehypeKatex,
+		],
+		remarkRehype: {
+			footnoteLabelTagName: "h1",
+			footnoteLabel: "脚注",
+			footnoteBackLabel: "Return to content",
+		},
+		gfm: true,
 	},
 
-  vite: {
-      css: {
-          preprocessorOptions: {
-              scss: {
-                  additionalData: `@import "src/assets/scss/base.scss";`,
-                  silenceDeprecations: ["legacy-js-api"],
-              },
-          },
-      },
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@import "src/assets/scss/base.scss";`,
+					silenceDeprecations: ["legacy-js-api"],
+				},
+			},
+		},
 	},
-
-  output: "static",
-  adapter: cloudflare(),
 });
