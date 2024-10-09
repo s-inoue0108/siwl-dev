6e415363f970989970f9c3964b9895da9d07107c
shota <inoueshota0108@gmail.com>
2024-10-09 23:08:24 +0900

    add adaptor forcloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,64 +20,67 @@ import rehypeInlineAnchor from "./src/plugins/rehype/rehype-inline-anchor";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	server: {
-		port: 3000,
+  server: {
+      port: 3000,
 	},
 
-	site: "https://siwl.dev",
+  site: "https://siwl.dev",
 
-	integrations: [
-		solid(),
-		sitemap({
-			i18n: {
-				defaultLocale: "ja",
-				locales: {
-					ja: "ja-JP",
-				},
-			},
-		}),
-		partytown({
-			// Adds dataLayer.push as a forwarding-event.
-			config: {
-				forward: ["dataLayer.push"],
-			},
-		}),
-		tailwind(),
+  integrations: [
+      solid(),
+      sitemap({
+          i18n: {
+              defaultLocale: "ja",
+              locales: {
+                  ja: "ja-JP",
+              },
+          },
+      }),
+      partytown({
+          // Adds dataLayer.push as a forwarding-event.
+          config: {
+              forward: ["dataLayer.push"],
+          },
+      }),
+      tailwind(),
 	],
 
-	markdown: {
-		syntaxHighlight: "shiki",
-		shikiConfig: {
-			theme: "dracula-soft",
-		},
-		remarkPlugins: [[rlc, { shortenUrl: true }], remarkDirective, remarkMath, remarkCodeTitles],
-		rehypePlugins: [
-			rehypeRaw,
-			[rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }],
-			rehypeInlineAnchor,
-			rehypeHeadings,
-			rehypeKatex,
-		],
-		remarkRehype: {
-			footnoteLabelTagName: "h1",
-			footnoteLabel: "脚注",
-			footnoteBackLabel: "Return to content",
-		},
-		gfm: true,
+  markdown: {
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+          theme: "dracula-soft",
+      },
+      remarkPlugins: [[rlc, { shortenUrl: true }], remarkDirective, remarkMath, remarkCodeTitles],
+      rehypePlugins: [
+          rehypeRaw,
+          [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }],
+          rehypeInlineAnchor,
+          rehypeHeadings,
+          rehypeKatex,
+      ],
+      remarkRehype: {
+          footnoteLabelTagName: "h1",
+          footnoteLabel: "脚注",
+          footnoteBackLabel: "Return to content",
+      },
+      gfm: true,
 	},
 
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: `@import "src/assets/scss/base.scss";`,
-					silenceDeprecations: ["legacy-js-api"],
-				},
-			},
-		},
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: `@import "src/assets/scss/base.scss";`,
+                  silenceDeprecations: ["legacy-js-api"],
+              },
+          },
+      },
 	},
 
-	output: "static",
+  output: "static",
+  adapter: cloudflare(),
 });
