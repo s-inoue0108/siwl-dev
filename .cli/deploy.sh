8df07f0e09d8c950999fc729f34c0562e997a788
shota <inoueshota0108@gmail.com>
2025-01-04 10:07:34 +0900

    [2025-01-04 10:07] deploy from edit

--- .cli/deploy.sh
+++ .cli/deploy.sh
@@ -2,11 +2,24 @@
 
 current_branch=$(git branch --contains | cut -d " " -f 2)
 main_branch="main"
+now=$(date +"%Y-%m-%d %H:%M")
 
-if [[ "$current_branch" != "$main_branch" ]]; then
+if [[ "$current_branch" == "$main_branch" ]]; then
   echo "[ERROR] You can't deploy from [main] branch."
   echo "Please try 'git switch <branch-name>'."
   exit 1
 fi
 
-echo $current_branch
+echo -e "\e[1m\e[95m[$now] Deploying from $current_branch\e[0m"
+
+git add . >/dev/null
+git commit -m "[$now] deploy from $current_branch" >/dev/null
+git push origin "$current_branch" >/dev/null
+
+git switch "$main_branch" >/dev/null
+git merge "$current_branch" >/dev/null
+git push origin "$main_branch" >/dev/null
+
+git switch "$current_branch" >/dev/null
+
+echo -e "\e[1m\e[92mDeployed successfully.\e[0m"
