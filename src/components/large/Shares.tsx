82c646f50c4a1510b6dc07ce8752ae7958569cf2
shota <inoueshota0108@gmail.com>
2025-01-07 09:28:53 +0900

    [2025-01-07 09:28] Deploy to origin/main

--- src/components/large/Shares.tsx
+++ src/components/large/Shares.tsx
@@ -1,6 +1,9 @@
 import type { CollectionEntry } from "astro:content";
 import { SiFacebook, SiMisskey, SiTwitter, SiHatenabookmark } from "solid-icons/si";
+import { BsQrCode } from "solid-icons/bs";
 import Panel from "./Panel";
+import { isOpenQRModal, setIsOpenQRModal } from "../../utils/store/is-open-qrmodal";
+import QRModal from "./QRModal";
 
 interface Props {
 	appOwner: string;
@@ -18,46 +21,52 @@ const Shares = ({ appOwner, appUrl, title, slug }: Props) => {
 	const hatenaLink = `http://b.hatena.ne.jp/entry/${url}`;
 	const misskeyLink = `https://misskey-hub.net/share/?text=${text}&url=${url}&visibility=public&localOnly=0`;
 	return (
-		<Panel
-			title="Share"
-			isShowToggleButton={false}
-			Content={
-				<div class="flex items-center gap-6">
-					<a
-						href={facebookLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						class="text-muted-foreground text-2xl hover:text-foreground transition-colors duration-150"
-					>
-						<SiFacebook />
-					</a>
-					<a
-						href={xLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						class="text-muted-foreground text-2xl hover:text-foreground transition-colors duration-150"
-					>
-						<SiTwitter />
-					</a>
-					<a
-						href={hatenaLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						class="text-muted-foreground text-2xl hover:text-foreground transition-colors duration-150"
-					>
-						<SiHatenabookmark />
-					</a>
-					<a
-						href={misskeyLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						class="text-muted-foreground text-lg sm:text-xl lg:text-2xl hover:text-foreground transition-colors duration-150"
-					>
-						<SiMisskey />
-					</a>
-				</div>
-			}
-		/>
+		<>
+			<Panel
+				title="Share"
+				isShowToggleButton={false}
+				Content={
+					<div class="flex flex-wrap items-center gap-6">
+						<a
+							href={facebookLink}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="text-muted-foreground text-2xl hover:text-foreground transition-colors duration-150"
+						>
+							<SiFacebook />
+						</a>
+						<a
+							href={xLink}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="text-muted-foreground text-2xl hover:text-foreground transition-colors duration-150"
+						>
+							<SiTwitter />
+						</a>
+						<a
+							href={hatenaLink}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="text-muted-foreground text-2xl hover:text-foreground transition-colors duration-150"
+						>
+							<SiHatenabookmark />
+						</a>
+						<a
+							href={misskeyLink}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="text-muted-foreground text-lg sm:text-xl lg:text-2xl hover:text-foreground transition-colors duration-150"
+						>
+							<SiMisskey />
+						</a>
+						<button type="button" onClick={() => setIsOpenQRModal(true)}>
+							<BsQrCode class="text-muted-foreground text-2xl hover:text-foreground transition-colors duration-150" />
+						</button>
+					</div>
+				}
+			/>
+			<QRModal appUrl={appUrl} slug={slug} />
+		</>
 	);
 };
 
