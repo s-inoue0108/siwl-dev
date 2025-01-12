c1455ddb275258bf14006b3f2cea2910ea37e7ef
shota <inoueshota0108@gmail.com>
2025-01-12 21:50:41 +0900

    [2025-01-12 21:50] Deploy to origin/main

--- src/plugins/rehype/rehype-code-copy-button.ts
+++ src/plugins/rehype/rehype-code-copy-button.ts
@@ -22,26 +22,40 @@ export default function rehypeCodeCopyButton() {
           viewBox: '0 0 512 512',
           height: '1em',
           width: '1em',
-          style: 'overflow: visible; color: currentcolor;',
+          style: "overflow: 'visible', color: 'currentcolor'",
         },
         children: [
           {
             type: 'element',
-            tagName: 'path',
+            tagName: 'rect',
             properties: {
-              d: 'M408 480H184a72 72 0 0 1-72-72V184a72 72 0 0 1 72-72h224a72 72 0 0 1 72 72v224a72 72 0 0 1-72 72Z',
+              width: '336',
+              height: '336',
+              x: '128',
+              y: '128',
+              fill: 'none',
+              stroke: 'currentColor',
+              'stroke-linejoin': 'round',
+              'stroke-width': '32',
+              rx: '57',
+              ry: '57',
             },
-            children: [],
+            children: []
           },
           {
             type: 'element',
             tagName: 'path',
             properties: {
-              d: 'M160 80h235.88A72.12 72.12 0 0 0 328 32H104a72 72 0 0 0-72 72v224a72.12 72.12 0 0 0 48 67.88V160a80 80 0 0 1 80-80Z',
+              fill: 'none',
+              stroke: 'currentColor',
+              'stroke-linecap': 'round',
+              'stroke-linejoin': 'round',
+              'stroke-width': '32',
+              d: 'm383.5 128 .5-24a56.16 56.16 0 0 0-56-56H112a64.19 64.19 0 0 0-64 64v216a56.16 56.16 0 0 0 56 56h24',
             },
-            children: [],
-          },
-        ],
+            children: []
+          }
+        ]
       } satisfies ElementContent;
 
       const copyButton = {
