b02dd28e10c2318a1aa535210ad9e74f15bb347d
shota <inoueshota0108@gmail.com>
2025-01-15 20:50:42 +0900

    [2025-01-15 20:50] Deploy to origin/main

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,24 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { type APIContext } from "astro";
+import { AllowedRoutes } from "../route";
+
+const routes = new AllowedRoutes("/");
+const { meta } = routes.getRootPageMeta();
 
 export async function GET(context: APIContext) {
 	const articles = await getCollection("article", ({ data }) => {
-		return import.meta.env.PROD ? data.isDraft === false : true;
+		return import.meta.env.PROD ? !data.isDraft && !data.isLimited : true;
 	});
 
 	return rss({
 		title: import.meta.env.APP_NAME,
-		description: "プログラミングについての情報を発信しています。",
+		description: meta.description,
 		site: context.site ?? import.meta.env.APP_URL,
 		items: articles.map(({ slug, data }) => ({
 			title: data.title,
 			pubDate: data.publishDate,
 			description: data.description ?? "",
-
 			link: `/blog/articles/${slug}`,
 		})),
 	});
