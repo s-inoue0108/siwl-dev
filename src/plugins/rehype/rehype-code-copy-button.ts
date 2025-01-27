956206d1fd8b4b956dcd010a92b4401b0f717908
shota <inoueshota0108@gmail.com>
2025-01-27 23:38:38 +0900

    [2025-01-27 23:38] Deploy to origin/main

--- src/plugins/rehype/rehype-code-copy-button.ts
+++ src/plugins/rehype/rehype-code-copy-button.ts
@@ -12,7 +12,7 @@ export default function rehypeCodeCopyButton() {
 
       count++;
 
-      const svg = {
+      const svgNode = {
         type: 'element',
         tagName: 'svg',
         properties: {
@@ -58,6 +58,10 @@ export default function rehypeCodeCopyButton() {
         ]
       } satisfies ElementContent;
 
+      const copySvgTag = `<svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="1em" width="1em" style="overflow: visible; color: currentcolor;"><rect width="336" height="336" x="128" y="128" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" rx="57" ry="57"></rect><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="m383.5 128 .5-24a56.16 56.16 0 0 0-56-56H112a64.19 64.19 0 0 0-64 64v216a56.16 56.16 0 0 0 56 56h24"></path></svg>`;
+
+      const checkSvgTag = `<svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="1em" width="1em" style="overflow: visible; color: currentcolor;"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M416 128 192 384 96 288"></path></svg>`;
+
       const copyButton = {
         type: 'element',
         tagName: 'button',
@@ -65,7 +69,7 @@ export default function rehypeCodeCopyButton() {
           className: "code-copy-button",
           id: `code-copy-button-${count}`,
         },
-        children: [svg],
+        children: [svgNode],
       } satisfies ElementContent;
 
       const script = {
@@ -82,6 +86,10 @@ export default function rehypeCodeCopyButton() {
               copyButton.addEventListener('click', () => {
                 const codeBlock = copyButton.previousElementSibling.children[0];
                 navigator.clipboard.writeText(codeBlock.innerText);
+                copyButton.innerHTML = '${checkSvgTag}';
+                setTimeout(() => {
+                  copyButton.innerHTML = '${copySvgTag}';
+                }, 1000);
               });
             });
           `
